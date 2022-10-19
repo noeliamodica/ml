ed813aca95ff25d98e7dc6bceba2a55ea3bea432
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2022-10-19 09:08:39 -0300

    subo ml

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+const publicFolderPath = path.resolve ('public');
+app.use(express.static(publicFolderPath));
+app.use (express.json());
+app.use (express.urlencoded());
+const APP_PORT = 1234;
+app.listen (APP_PORT, () => {
+    console.log('Servidor funcionando en puerto ' + APP_PORT)
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('views/register.html'))
+})
+
+app.get('/searchbar', (req, res) => {
+    res.sendFile(path.resolve('views/searchbar.html'))
+})
+
+
+app.post('/', (req, res) => {
+  return res.send (req.body);
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('views/login.html'))
+})
+; 
